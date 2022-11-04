b19a1c8abde69471b4e1b93120749ce51c8c2921
Rahul Ahire <info@rahulahire.com>
2022-11-04 20:11:07 +0530

    initialised sphere in Nasa Black Marble Page and testing for deployment

--- pages/nasa-black-marble/index.js
+++ pages/nasa-black-marble/index.js
@@ -1,7 +1,21 @@
+import { Canvas } from "@react-three/fiber";
+import {OrbitControls} from '@react-three/drei'
+import styles from "./styles/main.module.css";
 export default function index() {
   return (
-    <div>
-      <h1>Hello Nasa Black Marble</h1>
-    </div>
+    <>
+      <section className={styles.container}>
+        <div className={styles.canvas_area}>
+          <Canvas>
+          <ambientLight scale={0.5} />
+            <mesh>
+              <sphereBufferGeometry />
+              <meshStandardMaterial />
+            </mesh>
+              <OrbitControls />
+          </Canvas>
+        </div>
+      </section>
+    </>
   );
 }
