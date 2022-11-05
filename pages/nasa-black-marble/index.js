39148baa9807e432a68a92183dcb22bd12becba4
Rahul Ahire <info@rahulahire.com>
2022-11-06 02:57:09 +0530

    bought all the threejs code in index.js to see if this working properly in deployment

--- pages/nasa-black-marble/index.js
+++ pages/nasa-black-marble/index.js
@@ -1,7 +1,8 @@
 import { Canvas } from "@react-three/fiber";
-import Earth from "./component/earth";
+// import Earth from "./component/earth";
 import styles from "./styles/main.module.css";
 import OpenLayers from "./component/openLayers";
+import { OrbitControls, useTexture } from "@react-three/drei";
 export default function index() {
   return (
     <>
@@ -15,4 +16,18 @@ export default function index() {
       <OpenLayers />
     </>
   );
+}
+export  function Earth() {
+  const [colorMap] = useTexture(['img/bhca3.png'])
+return (
+  <>
+    <mesh>
+      <ambientLight intensity={1}/>
+      <directionalLight position={[0,5,5]}/>
+      <sphereGeometry args={[10, 64, 64]} />
+      <meshStandardMaterial color={0xffffae} map={colorMap}/>
+    </mesh>
+    <OrbitControls/>
+  </>
+);
 }
