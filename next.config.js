120f6c557f4f1101e91412c71214454d5bdcc842
Rahul Ahire <info@rahulahire.com>
2022-11-15 14:29:44 +0530

    added transpile module for ol in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
 };
 const withTM = require("next-transpile-modules")(["three"]);
-
+const withTM2 = require("next-transpile-modules")(["ol"]);
 
 module.exports = withTM();
+module.exports = withTM2();
 module.exports = nextConfig;
--- pages/nasa-black-marble/index.js
+++ pages/nasa-black-marble/index.js
@@ -1,16 +1,9 @@
 import { Canvas } from "@react-three/fiber";
-// import Earth from "../../component/nasa-black-marble/earth";
+import Earth from "../../component/nasa-black-marble/earth";
 import styles from "./styles/main.module.css";
-// import OpenLayers from "../../component/nasa-black-marble/openLayers";
+import OpenLayers from "../../component/nasa-black-marble/openLayers";
 import { Suspense } from "react";
-import dynamic from "next/dynamic";
 export default function index() {
-  const OpenLayers = dynamic(() => import('../../component/nasa-black-marble/openLayers'), {
-    ssr: false,
-  })
-  const Earth = dynamic(() => import('../../component/nasa-black-marble/earth'), {
-    ssr: false,
-  })
   return (
     <>
       <section className={styles.container}>
