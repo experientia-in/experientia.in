2c5b152428d848a532560a639d5d2bd186034df2
Rahul Ahire <info@rahulahire.com>
2022-11-21 17:31:07 +0530

    disabled swcMinify to false and testing on deployment if openlayers works or not

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false
 };
 const withTM = require("next-transpile-modules")(["three"]);
 const withTM2 = require("next-transpile-modules")(["ol"]);
