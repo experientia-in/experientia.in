276672eac379a99f92adad07a1b1fd60e18edae0
Rahul Ahire <info@rahulahire.com>
2022-12-01 18:39:13 +0530

    testing animation on deployment

--- src/nasa-black-marble/comp/overlay.jsx
+++ src/nasa-black-marble/comp/overlay.jsx
@@ -1,27 +1,39 @@
-import styles from "../style/overlay.module.css";
+import gsap, { Back } from "gsap";
+import { useLayoutEffect } from "react";
+import "../style/overlay.css";
 
 export default function Overlay() {
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(".mapCards", { duration: 1, translateX: "0%", stagger: 0.1 });
+      gsap.to(".mapCards_m", { duration: 0.5, translateY: "0%", stagger: 0.1 });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
   return (
     <>
-      <section className={styles.overlay_wrapper}>
-        <div className={styles.intro}>
-          <div className={styles.bigScreen}>
-            <div className={styles.streetMap}></div>
-            <div className={styles.basicMap}></div>
-            <div className={styles.outdoorMap}></div>
-            <div className={styles.darkMonoChromeMap}></div>
-            <div className={styles.physicalMap}></div>
-            <div className={styles.whiteMonoChromeMap}></div>
-            <div className={styles.winterMap}></div>
+      <section className="overlay_wrapper">
+        <div className="intro">
+          <div className="bigScreen">
+            <div className="streetMap mapCards"></div>
+            <div className="basicMap mapCards"></div>
+            <div className="outdoorMap mapCards"></div>
+            <div className="darkMonoChromeMap mapCards"></div>
+            <div className="physicalMap mapCards"></div>
+            <div className="whiteMonoChromeMap mapCards"></div>
+            <div className="winterMap mapCards"></div>
           </div>
-          <div className={styles.smallScreen}>
-            <div className={styles.streetMap_m}></div>
-            <div className={styles.basicMap_m}></div>
-            <div className={styles.outdoorMap_m}></div>
-            <div className={styles.darkMonoChromeMap_m}></div>
-            <div className={styles.physicalMap_m}></div>
-            <div className={styles.whiteMonoChromeMap_m}></div>
-            <div className={styles.winterMap_m}></div>
+          <div className="smallScreen">
+            <div className="streetMap_m mapCards_m"></div>
+            <div className="basicMap_m mapCards_m"></div>
+            <div className="outdoorMap_m mapCards_m"></div>
+            <div className="darkMonoChromeMap_m mapCards_m"></div>
+            <div className="physicalMap_m mapCards_m"></div>
+            <div className="whiteMonoChromeMap_m mapCards_m"></div>
+            <div className="winterMap_m mapCards_m"></div>
           </div>
         </div>
       </section>
