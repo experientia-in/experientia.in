ca19f3ee7fd04edd7745839d08fcd95a2315a4ab
Rahul Ahire <info@rahulahire.com>
2022-12-02 16:54:34 +0530

    added a really simple loader

---
+++ src/nasa-black-marble/comp/loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <>
+        <h1 style={{color: 'red', position: 'absolute', zIndex :'1000'}}>loading</h1>
+    </>
+  )
+}
