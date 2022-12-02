6b0e339d1dc7dc17848d61e7f41dbaa499d47a9e
Rahul Ahire <info@rahulahire.com>
2022-12-02 17:14:28 +0530

    fixed the background image issue and now trying it live

--- src/nasa-black-marble/comp/loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loader() {
-  return (
-    <>
-        <h1 style={{color: 'red', position: 'absolute', zIndex :'1000'}}>loading</h1>
-    </>
-  )
-}
--- src/nasa-black-marble/index.jsx
+++ src/nasa-black-marble/index.jsx
@@ -4,13 +4,11 @@ import Earth from "./comp/earth";
 import OpenLayers from "./comp/openLayers";
 import styles from "./style/main.module.css";
 import Overlay from "./comp/overlay";
-import Loader from "./comp/loader";
 export default function BlackMarble() {
   const [canvasLoaded, setCanvasLoaded] = useState(false)
   
   return (
     <>
-    {!canvasLoaded && <Loader/>}
       <section className={styles.container}>
         <div className={styles.canvas_area}>
           <Canvas linear flat>
