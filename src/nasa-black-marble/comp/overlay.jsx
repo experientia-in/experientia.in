1e7a66c6866f4ea44cb0c82bbe93880855593ee0
Rahul Ahire <info@rahulahire.com>
2022-12-06 00:44:50 +0530

    added all the country for desktop mode, move on towards openlayers optimisation

--- src/nasa-black-marble/comp/overlay.jsx
+++ src/nasa-black-marble/comp/overlay.jsx
@@ -1,8 +1,10 @@
 import gsap from "gsap";
+import { useState } from "react";
 import { useLayoutEffect } from "react";
 import "../style/overlay.css";
 
 export default function Overlay() {
+  const [year, setYear] = useState('2016')
   useLayoutEffect(() => {
     const ctx = gsap.context(() => {
       gsap.to(".mapCards", {
@@ -116,7 +118,177 @@ export default function Overlay() {
               <img src="https://static.experientia.in/nasaBlackMarble/afghanistan_mountain.webp" alt="country_image_feature" />
             </div>
             <div className="country_feature_info">
-              On map, Afghanistan may look like a decent size country but majority of its land is covered by tall mountains in central area some of them ranging more than 7Km. Cities in Afghanistan mostly form a ring pattern which are connected by highways.
+              On map, Afghanistan may look like a decent size country but majority of its land is covered by tall mountains in central area, some of them ranging more than 7Km. Cities in Afghanistan mostly form a ring pattern which are connected by highways.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Iceland
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/iceland_glacier.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Similar to Afghanistan, vast area in Iceland are covered by glaciers and Ice caps making it unsuitable for human activity. Majority of Iceland's population live in its capital city Reykjavík.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Argentina
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/argentina_messi.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Perhaps you may recognise Argentina by Leonal Messi. The population distribution of Argentines is rather interesting as most of cities and town are situated around the railway station that seems evenly spread out throughout the country.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Russia
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/russia_doll.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              If you believe size matters then Russia is of your kind, spanning across 9,000Km and 11 time zones which obviously make it the largest country on our planet. Learn more about it <a href="https://youtu.be/HBlZlmXyR5M" target="_blank" title="
+Why is Russia So DAMN BIG?" rel="noopener noreferrer">here</a>.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Australia
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/australia_kangaroo.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              For the land as big as Australia, very few people actually live on this continent due to most of it land being a dry desert making it one of the least densely populated country. 90% of Australians live in the costal areas.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            USA
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/usa_nycStatue.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              In terms of population distribution, USA truly reflect its East vs West divide. The tall rocky mountains in the central region stop the rainy wind travelling war in the West as compared to the East side where land is more fertile thus supporting many lives. 
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Egypt
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/egypt_pyramid.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Egypt being situated in amidst of Desert, the river Nile support almost every Egyptian's lifeline as a result almost 90% of Egypt's population live near to the river.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Pakistan
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/pakistan_mohenJoDaro.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Similar to Egypt, most Pakistanis live around the Indus river which carries fresh water from himalyas.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            India <span className="country_year">{year}</span>
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/india_bharatnatyam.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              India, the land of cultural diversity and also perhaps the most brightest nation in the night. The difference between 2012 and 2016 Nasa's Black Map edition in content to India is shows us the drastic change as just in span of 4 years 100's of Million have got access to electricity which previous they were deprived of. Learn more about it <a href="https://www.macrotrends.net/countries/IND/india/electricity-access-statistics" target='_blank'>here</a>. 
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            China <span className="country_year">{year}</span>
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/china_greatWallOfChina.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Compared to India, the case for China has been completed opposite. Compared to 2012, The 2016's version of Map appears to be more dimmer as many people in have moved toward the Urban areas in search for jobs.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Africa
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/africa_wildlife.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Country's economic often tell us the living standard of people and normally African aren't that rich compared to other nation and since most of them don't have access to electricity, this picture tells us why Africa is the way it is. 
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Korea
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/korea_bts.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              In recent times, the korean culture may have got popular of K-POP, films, etc. The effect of cold war with USA and Soviets still reflects till today in form in North and South Korea diving people with same background, language, culture, etc. It also signifies the progress of democratic South Korea as opposed to authoritarian regime in North Korea. Learn more about it <a href="https://youtu.be/Jt7hE12n11s" target="_blank" rel="noopener noreferrer"></a>.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Yemen <span className="country_year">{year}</span>
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/yemen_people.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              The middle-East has always been unstable due to its internal conflit. Compared to 2012, the 2016's map show us how Yemenese have been deeply affect as many of property have been succumbed to mass destruction.
+            </div>
+          </div>
+        </div>
+        <div className="country_section">
+          <div className="country_name">
+            Syria <span className="country_year">{year}</span>
+          </div>
+          <div className="country_card">
+            <div className="country_feature_image">
+              <img src="https://static.experientia.in/nasaBlackMarble/syria_war.webp" alt="country_image_feature" />
+            </div>
+            <div className="country_feature_info">
+              Simailar has been the case for Syrians as many of them have to leave their home in order to survive.
             </div>
           </div>
         </div>
