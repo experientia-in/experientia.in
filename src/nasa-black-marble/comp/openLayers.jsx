ad7f7c9e5f6d754afb5d96631bf855f431abb783
Rahul Ahire <info@rahulahire.com>
2022-12-11 20:46:50 +0530

    uploading it to get the resolution answer from drmcda and other as texture is not crisper disabled additional map layers

--- src/nasa-black-marble/comp/openLayers.jsx
+++ src/nasa-black-marble/comp/openLayers.jsx
@@ -24,22 +24,22 @@ export default function OpenLayers() {
       }),
     });
     map.addLayer(image_2016)
-    const iceland = new ImageLayer({
-      source: new Static({
-        url: "/assets/img/nasaBlackMarble/iceland.webp",
-        interpolate: false,
-        imageExtent: [-24.878805226,62.741177263,-13.066305226,67.298154634],
-      }),
-    });
-    map.addLayer(iceland);
-    const egypt_middleEast = new ImageLayer({
-      source: new Static({
-        url: "/assets/img/nasaBlackMarble/egypt_middleEast_2016.webp",
-        interpolate: false,
-        imageExtent: [28.000000000,12.345833333,61.195833333,38.212500000],
-      }),
-    });
-    map.addLayer(egypt_middleEast)
+    // const iceland = new ImageLayer({
+    //   source: new Static({
+    //     url: "/assets/img/nasaBlackMarble/iceland.webp",
+    //     interpolate: false,
+    //     imageExtent: [-24.878805226,62.741177263,-13.066305226,67.298154634],
+    //   }),
+    // });
+    // map.addLayer(iceland);
+    // const egypt_middleEast = new ImageLayer({
+    //   source: new Static({
+    //     url: "/assets/img/nasaBlackMarble/egypt_middleEast_2016.webp",
+    //     interpolate: false,
+    //     imageExtent: [28.000000000,12.345833333,61.195833333,38.212500000],
+    //   }),
+    // });
+    // map.addLayer(egypt_middleEast)
     
   }, []);
   return (
